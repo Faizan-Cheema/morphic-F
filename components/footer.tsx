bb67403e8154e05087ea64cf665bcede9f16f283
ifti96 <91176241+ifti96@users.noreply.github.com>
2025-06-13 01:40:15 +0500

    footer fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,9 +5,9 @@ const Footer: React.FC<{ isVisible: boolean }> = ({ isVisible }) => {
   if (!isVisible) return null;
 
   return (
-    <footer className="fixed bottom-0 left-0 right-0 w-full p-1 md:p-2 hidden lg:block">
+    <footer className="fixed bottom-0 left-0 right-0 w-full hidden lg:block">
       <div className="flex justify-center items-center">
-        <div className='text-sm text-gray-500'>
+        <div className='text-xs text-gray-500'>
           The AI Manager can make mistakes. Check important info.
         </div>
       </div>
